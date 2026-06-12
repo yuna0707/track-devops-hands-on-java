09f35a71a9dfeca7f08156295deef7db5319d264
yuna0707 <yuna.ohtsuki@tcs.com>
2026-06-12 13:51:55 +0900

    Refactor CalculatorTest to include decimal tests

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -2,9 +2,14 @@ package com.example.handson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class CalculatorTest {
-@Test
-  public void testAdd() {
+  @Test
+  public void testAddIntegerValues() {
     Calculator calc = new Calculator();
-    assertEquals(5, calc.add(2, 3));
+    assertEquals(5.0, calc.add(2.0, 3.0));
+  }
+  @Test
+  public void testAddDecimalValues() {
+    Calculator calc = new Calculator();
+    assertEquals(4.8, calc.add(1.2, 3.5), 0.0001);
   }
 }
